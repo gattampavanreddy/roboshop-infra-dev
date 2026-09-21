b42b3d64b447a1a9d2fada5bbda87b65e001a270
GattamPavan <pavanreddy2266@gmail.com>
2026-09-21 18:14:18 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -85,3 +85,44 @@ resource "terraform_data" "bootstrap_redis" {
         ]
     }
 }
+
+#=========================================================
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro" 
+  subnet_id     = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+  
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-mysql"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "mysql" {
+    triggers_replace = [
+        aws_instance.mysql.id
+    ]
+
+    connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mysql.private_ip
+    }
+
+    provisioner "file" {
+        source = "bootstarp.sh" #local file path 
+        destination = "/tmp/bootstarp.sh" #destination file path on the remote instance
+    }
+  
+    provisioner "remote-exec" { 
+        inline = [
+            "chmod +x /tmp/bootstarp.sh",
+            "sudo sh /tmp/bootstarp.sh mysql"
+        ]
+    }
+}
