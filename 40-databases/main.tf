dc4e924144ae3faf1f3c40daae58b673d365715f
GattamPavan <pavanreddy2266@gmail.com>
2026-09-22 19:20:51 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -127,3 +127,45 @@ resource "terraform_data" "mysql" {
         ]
     }
 }
+
+#=========================================================
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro" 
+  subnet_id     = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  iam_instance_profile = aws_iam_instance_profile.rabbitmq.name
+  
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+    triggers_replace = [
+        aws_instance.rabbitmq.id
+    ]
+
+    connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.rabbitmq.private_ip
+    }
+
+    provisioner "file" {
+        source = "bootstarp.sh" #local file path 
+        destination = "/tmp/bootstarp.sh" #destination file path on the remote instance
+    }
+  
+    provisioner "remote-exec" { 
+        inline = [
+            "chmod +x /tmp/bootstarp.sh",
+            "sudo sh /tmp/bootstarp.sh rabbitmq ${var.environment}"
+        ]
+    }
+}
