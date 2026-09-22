e825dffd8c00d2323b939c6fb2b2ee51677d7e44
GattamPavan <pavanreddy2266@gmail.com>
2026-09-22 18:21:38 +0530

    terraform

--- 40-databases/bootstarp.sh
+++ 40-databases/bootstarp.sh
@@ -9,4 +9,4 @@ git clone https://github.com/gattampavanreddy/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull 
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$var.environment roboshop.yaml
