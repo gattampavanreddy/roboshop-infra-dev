2a199f59593711e52dcaa83aa89e5668e55c2045
GattamPavan <pavanreddy2266@gmail.com>
2026-09-22 23:36:14 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -49,12 +49,12 @@ resource "aws_security_group_rule" "redis_bastion" {
   security_group_id = local.redis_sg_id
 }
 
-resource "aws_security_group_rule" "mysql_bastion" {
+resource "aws_security_group_rule" "rabbitmq_bastion" {
   type        = "ingress"
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
   #where the traffic is comming from, replacing CIDR with source SG ID
   source_security_group_id = local.bastion_sg_id
-  security_group_id = local.mysql_sg_id
+  security_group_id = local.rabbitmq_sg_id
 }
