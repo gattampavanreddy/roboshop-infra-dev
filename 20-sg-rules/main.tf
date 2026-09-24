d7e2ea50e1f7e3468e19806d05d305a4c983e7fc
GattamPavan <pavanreddy2266@gmail.com>
2026-09-24 19:35:14 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -58,3 +58,13 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.rabbitmq_sg_id
 }
+
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  #where the traffic is comming from, replacing CIDR with source SG ID
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
